867d9dd99a3538e1e248d14c65b84ec7d2a659c3
David Jackson <davidjackson123@gmail.com>
2025-07-20 00:53:39 -0400

    update icons

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -16,7 +17,33 @@ export default function App() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BudgetProvider>
         <NavigationContainer>
-          <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                switch (route.name) {
+                  case 'Home':
+                    iconName = 'home';
+                    break;
+                  case 'Transactions':
+                    iconName = 'receipt-long';
+                    break;
+                  case 'Recurring':
+                    iconName = 'repeat';
+                    break;
+                  case 'Accounts':
+                    iconName = 'account-balance-wallet';
+                    break;
+                  default:
+                    iconName = 'circle';
+                }
+
+                return <MaterialIcons name={iconName} size={size} color={color} />;
+              }
+            })}
+          >
             <Tab.Screen name="Home" component={HomeStack} />
             <Tab.Screen name="Transactions" component={TransactionsStack} />
             <Tab.Screen name="Recurring" component={RecurringStack} />
